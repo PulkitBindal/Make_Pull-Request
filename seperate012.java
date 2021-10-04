d326964c0078f7411ed7fff6348f78a9497f1627
PulkitBindal <67583540+PulkitBindal@users.noreply.github.com>
2021-10-04 16:11:35 +0530

    Add files via upload

---
+++ seperate012.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner.*;
+// Used Dutch National Flag Approach 
+class solution{
+	void fun(int[] A,int len){
+		// Logic
+	}
+}
+
+public class seperate012{	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		int count0=0,count1=0,count2=0;
+		int[] A = new int[100];
+		for(int i=0;i<len;i++){
+			A[i]=sc.nextInt();
+		}
+		solution sl = new solution();
+		sl.fun(A,len);
+	}
+}
